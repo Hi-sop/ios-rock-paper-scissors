69127ed92b40c43fcc03eb7fb51b03dc69076371
jyubong <nane030@naver.com>
2023-09-05 19:47:42 +0900

    그냥 구현해봄

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -4,48 +4,122 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
-
-func startGame() {
-    var isPlaying: Bool = true
-    
-    while isPlaying {
-        print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
-        
-        guard let userChoice = readLine() else {
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-            continue
-        }
-        
-        switch userChoice {
-        case "1", "2", "3":
-            isPlaying = makeResult(userChoice: userChoice)
-        case "0":
-            print("게임 종료")
-            return
-        default:
-            print("잘못된 입력입니다. 다시 시도해주세요.")
-        }
-    }
+enum Choice: String {
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
 }
 
-func makeResult(userChoice: String) -> Bool {
-    let computerChoice: String = String(Int.random(in: 1...3))
-    
+enum Winner: String {
+    case user = "사용자"
+    case computer = "컴퓨터"
+}
+
+@discardableResult
+func startGame(continueRecursive: Bool) -> Bool {
+    guard continueRecursive else {
+    return false
+    }
+
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
+
+    guard let userChoice = readLine() else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return startGame(continueRecursive: true)
+    }
+
+    switch userChoice {
+    case "1", "2", "3":
+        startGame(continueRecursive: compareWithComputer(userChoice: userChoice))
+    case "0":
+        print("게임 종료")
+        break
+    default:
+        startGame(continueRecursive: true)
+    }
+
+    return false
+
+}
+
+func compareWithComputer(userChoice: String) -> Bool {
+    let computerNumber: String = String(Int.random(in: 1...3))
+
+    guard let computerChoice: Choice = Choice(rawValue: computerNumber),
+          let userChoice: Choice = Choice(rawValue: userChoice) else {
+        return true
+    }
+
     guard computerChoice != userChoice else {
         print("비겼습니다!")
         return true
     }
-    
-    if (computerChoice == "1" && userChoice == "3") ||
-        (computerChoice == "2" && userChoice == "1") ||
-        (computerChoice == "3" && userChoice == "2") {
-        print("졌습니다!")
-        return false
-    } else {
+
+    guard (computerChoice == .scissors && userChoice == .paper) ||
+            (computerChoice == .rock && userChoice == .scissors) ||
+            (computerChoice == .paper && userChoice == .rock) else {
         print("이겼습니다!")
-        return false
+        return playSecondGame(continueRecursive: true, turn: .user)
     }
+
+    print("졌습니다!")
+    return playSecondGame(continueRecursive: true, turn: .computer)
+
 }
 
-startGame()
+@discardableResult
+func playSecondGame(continueRecursive: Bool, turn: Winner) -> Bool{
+    guard continueRecursive else {
+    return false
+    }
+
+    print("[\(turn.rawValue) 턴] 묵(1), 찌(2), 빠(3)! <종료 : 0> : ", terminator: "")
+
+    guard let userChoice = readLine() else {
+        print("잘못된 입력입니다. 다시 시도해주세요.")
+        return playSecondGame(continueRecursive: true, turn: .computer)
+    }
+
+    switch userChoice {
+    case "1", "2", "3":
+        let (bool,winner) = winOrLose(userChoice: userChoice, turn: turn)
+        playSecondGame(continueRecursive: bool, turn: winner)
+    case "0":
+        print("게임 종료")
+        break
+    default:
+        playSecondGame(continueRecursive: true, turn: .computer)
+    }
+
+    return false
+}
+
+func winOrLose(userChoice: String, turn: Winner) -> (Bool, Winner) {
+    let computerNumber: String = String(Int.random(in: 1...3))
+
+    guard let computerChoice: Choice = Choice(rawValue: computerNumber),
+          let userChoice: Choice = Choice(rawValue: userChoice) else {
+        return (true, .computer)
+    }
+
+    guard computerChoice != userChoice else {
+        print("""
+        같은 패입니다.
+        \(turn.rawValue)의 승리!
+        """)
+        return (false, turn)
+    }
+
+    guard (computerChoice == .scissors && userChoice == .paper) ||
+            (computerChoice == .rock && userChoice == .scissors) ||
+            (computerChoice == .paper && userChoice == .rock) else {
+        print("이겼습니다!")
+        return (true, .user)
+    }
+
+    print("졌습니다!")
+    return (true, .computer)
+
+}
+
+startGame(continueRecursive: true)
